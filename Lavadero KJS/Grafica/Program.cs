861f7bc52dbaea99e7238c4566c1683c97c42cc5
Stevenmolina05 <stevenjotamolina@gmail.com>
2023-06-11 18:19:13 -0500

    Consulta de usuarios terminada

--- Lavadero KJS/Grafica/Program.cs
+++ Lavadero KJS/Grafica/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
 
 namespace Presentacion
 {
@@ -12,24 +11,10 @@ namespace Presentacion
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
+
         [STAThread]
         static void Main()
         {
-            string server = "localhost";
-            string database = "lavadero";
-            string user = "root";
-            string pwd = "steven123";
-            string cadenaConexion = "server=" + server + ";database=" + database + ";Uid=" + user + ";pwd=" + pwd;
-
-            try
-            {
-                MySqlConnection mySqlConnection = new MySqlConnection(cadenaConexion);
-                mySqlConnection.Open();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
